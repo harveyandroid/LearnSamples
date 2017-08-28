360e2d1dff42ab73db5db3a98f37ab8dff992046
hanhui <hanhui@avatarcn.com>
2017-08-28 14:32:16 +0800

    Optional 测试修改

--- java-test/src/main/java/com/harvey/TestOptional.java
+++ java-test/src/main/java/com/harvey/TestOptional.java
@@ -20,5 +20,10 @@ public class TestOptional {
         System.out.println("Full Name: " + fullName.orElseGet(() -> "[none]"));
         System.out.println(fullName.map(s -> "Hey " + s + "!").orElse("Hey Stranger!"));
 
+        Optional< String > firstName = Optional.of( "Tom" );
+        System.out.println( "First Name is set? " + firstName.isPresent() );
+        System.out.println( "First Name: " + firstName.orElseGet( () -> "[none]" ) );
+        System.out.println( firstName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+        System.out.println();
     }
 }
